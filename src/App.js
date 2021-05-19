3b718fde1c5f1110340a559881f4505ff09756c5
Connor Czarnuch <connor.czarnuch@gmail.com>
2021-05-19 15:14:42 -0400

    fix homepage use hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import NavBar from './components/navbar';
 import HomePage from './pages/homepage';
 import About from './pages/about';
@@ -10,32 +10,34 @@ import Contact from './pages/contact';
 
 function App() {
   return (
-    <div className="App">
-      <div className="sidebar">
-        <NavBar />
-      </div>
-      <div className="main-content">
-        <div className="content">
-          <Switch>
-            <Route path="/home" exact>
-              <HomePage />
-            </Route>
-            <Route path="/about" exact>
-              <About />
-            </Route>
-            <Route path="/skills" exact>
-              <Skills />
-            </Route>
-            <Route path="/projects" exact>
-              <Projects />
-            </Route>
-            <Route path="/contact" exact>
-              <Contact />
-            </Route>
-          </Switch>
+    <HashRouter basename='/'>
+      <div className="App">
+        <div className="sidebar">
+          <NavBar />
+        </div>
+        <div className="main-content">
+          <div className="content">
+            <Switch>
+              <Route path="/" exact>
+                <HomePage />
+              </Route>
+              <Route path="/about" exact>
+                <About />
+              </Route>
+              <Route path="/skills" exact>
+                <Skills />
+              </Route>
+              <Route path="/projects" exact>
+                <Projects />
+              </Route>
+              <Route path="/contact" exact>
+                <Contact />
+              </Route>
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
+    </HashRouter>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,7 +12,7 @@ function NavBar() {
 
                 <ul className="nav-items">
                     <li className="nav-item">
-                        <NavLink to="/home" exact activeClassName="active">
+                        <NavLink to="/" exact activeClassName="active">
                             Home
                         </NavLink>
                     </li>
