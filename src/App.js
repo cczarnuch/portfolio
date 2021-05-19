e1be87330b5842eb64161d5e60eed450708b24c1
Connor Czarnuch <connor.czarnuch@gmail.com>
2021-05-19 15:26:00 -0400

    reactify app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import './App.css';
+import React, { Component } from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import NavBar from './components/navbar';
 import HomePage from './pages/homepage';
@@ -8,37 +9,40 @@ import Skills from './pages/skills';
 import Projects from './pages/projects';
 import Contact from './pages/contact';
 
-function App() {
-  return (
-    <HashRouter basename='/'>
-      <div className="App">
-        <div className="sidebar">
-          <NavBar />
-        </div>
-        <div className="main-content">
-          <div className="content">
-            <Switch>
-              <Route path="/" exact>
-                <HomePage />
-              </Route>
-              <Route path="/about" exact>
-                <About />
-              </Route>
-              <Route path="/skills" exact>
-                <Skills />
-              </Route>
-              <Route path="/projects" exact>
-                <Projects />
-              </Route>
-              <Route path="/contact" exact>
-                <Contact />
-              </Route>
-            </Switch>
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename='/'>
+        <div className="App">
+          <div className="sidebar">
+            <NavBar />
+          </div>
+          <div className="main-content">
+            <div className="content">
+              <Switch>
+                <Route path="/" exact>
+                  <HomePage />
+                </Route>
+                <Route path="/about" exact>
+                  <About />
+                </Route>
+                <Route path="/skills" exact>
+                  <Skills />
+                </Route>
+                <Route path="/projects" exact>
+                  <Projects />
+                </Route>
+                <Route path="/contact" exact>
+                  <Contact />
+                </Route>
+              </Switch>
+            </div>
           </div>
         </div>
-      </div>
-    </HashRouter>
-  );
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
